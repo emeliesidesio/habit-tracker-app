b548f1844025c2d11cd5c5e332ce5a3c0237b267
Emelie Sidesiö <esidesio@gmail.com>
2017-12-22 09:45:24 +0100

    map over icons on form page

--- code/src/components/form/chooseicon.js
+++ code/src/components/form/chooseicon.js
@@ -1,19 +1,11 @@
 import React from "react"
-import carrot from "assets/carrot.png"
-import heart from "assets/heart.png"
-import cleaning from "assets/cleaning.png"
-import petfood from "assets/pet-food.png"
-import piggybank from "assets/piggy-bank.png"
-import leaf from "assets/leaf.png"
-import pint from "assets/pint.png"
-import strong from "assets/strong.png"
 
 export default class ChooseIcon extends React.Component {
   render() {
     return (
       <label className="radiolabel">
         <input className="radioinput" type="radio" name="icon" value="small" />
-        <img className="habiticon" src={this.props.img} alt="" />
+        <img className="habiticon" src={this.props.habitIcon} alt="" />
       </label>
     )
   }
