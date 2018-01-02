828786ef2461c28afff95eeb2536793d8fedfea1
Hilda <hilda.sandberg@gmail.com>
2018-01-02 14:02:24 +0100

    Revert "Merge branch 'master' of https://github.com/Technigo/sprint-4-company-2"
    This reverts commit 3077c2454bc36e98e7b95817b2b24f51c074aa68, reversing
    changes made to 51d337244852ab3e3284debe30bcd683fa634085.

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { BrowserRouter, Route } from "react-router-dom"
 import Form from "components/form/form.js"
 import Item from "components/item/item.js"
 import Header from "components/header/header.js"
@@ -14,63 +13,51 @@ class App extends React.Component {
       toDoItems: [{ id: 1, value: "+", checkedDates: ["2018-01-01", "2018-01-02"] }, { id: 2, value: "+" },
         { id: 3, value: "+" }, { id: 4, value: "+" },
         { id: 5, value: "+" }, { id: 6, value: "+" },
-        { id: 7, value: "+" }, { id: 8, value: "+" }
+        { id: 7, value: "+" }, { i: 8, value: "+" }
       ]
     }
   }
 
-  addItem = (item, done, times, radio) => {
-    console.log("got a new item", item)
-    console.log("it is done?", done)
-    console.log("I shall do it how many times? ", times)
-    console.log("I want the logo ", radio)
-    this.setState({
-      toDoItems: [...this.state.toDoItems, item]
-    }, () => {
-      console.log("The items are: ", this.state.toDoItems)
-    })
-  }
+   addItem = (item, done, times, radio) => {
+     console.log("got a new item", item)
+     console.log("it is done?", done)
+     console.log("I shall do it how many times? ", times)
+     console.log("I want the logo ", radio)
+     this.setState({
+       toDoItems: [...this.state.toDoItems, item]
+     }, () => {
+       console.log("The items are: ", this.state.toDoItems)
+     })
+   }
 
-  checkItem = (item, done) => {
-    console.log("this item is finished", item)
-    console.log("this item is done?", done)
-  }
+   checkItem = (item, done) => {
+     console.log("this item is finished", item)
+     console.log("this item is done?", done)
+   }
 
-  removeItem = item => {
-    this.setState({
-      toDoItems: this.state.toDoItems.filter(todo => todo !== item)
-    })
-  }
+   removeItem = item => {
+     this.setState({
+       toDoItems: this.state.toDoItems.filter(todo => todo !== item)
+     })
+   }
 
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="app-container">
-          {/* <Item /> */}
-          <Header />
-          <Route
-            exact
-            path="/form"
-            render={routeProps =>
-              <Form
-                {...routeProps}
-                addItemToList={this.addItem} />
-            } />
-          <div className="ItemsList">
-            <Route
-              exact
-              path="/"
-              render={routeProps =>
-                <List
-                  {...routeProps}
-                  toDoItems={this.state.toDoItems} />
-              } />
-          </div>
-          <Route path="/calendar" exact component={Calendar} />
-        </div>
-      </BrowserRouter>
-    )
-  }
+   render() {
+     return (
+       <div className="app-container">
+         {/* <Item /> */}
+         <Header />
+         <Form addItemToList={this.addItem} />
+         <div className="ItemsList">
+           <List
+             toDoItems={this.state.toDoItems}
+             checkItem={this.state.checkItem}
+             removeItem={this.state.removeItem} />
+         </div>
+         <Calendar
+           toDoItems={this.state.toDoItems} />
+       </div>
+     )
+   }
 }
 
 export default App
