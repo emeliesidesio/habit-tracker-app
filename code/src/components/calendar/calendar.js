f0563e1504830a21ab7cbed34785a03e99751481
Hilda <hilda.sandberg@gmail.com>
2018-01-03 11:33:00 +0100

    calendar code cleaning

--- code/src/components/calendar/calendar.js
+++ code/src/components/calendar/calendar.js
@@ -4,51 +4,25 @@ import dateArray from "moment-array-dates"
 import CalendarDays from "./calendarDays.js"
 import "./calendar.css"
 
-// const today = new Date()
-// const startOfMonth = moment().startOf("month")
-// const endOfMonth = moment().endOf("month")
-// const daysInMonth = dateArray.range(startOfMonth, endOfMonth, "YYYY-MM-DD", true)
-const startOfWeek = moment().startOf("week")
-const endOfWeek = moment().endOf("week")
+const startOfWeek = moment().startOf("week").add(1, "days")
+const endOfWeek = moment().endOf("week").add(1, "days")
 const daysInWeek = dateArray.range(startOfWeek, endOfWeek, "YYYY-MM-DD", true)
-// const momentToday = moment().format("dddd")
-// const daysAgoFromToday = moment().subtract(3, "days")
-// const daysFromToday = moment().add(3, "days")
-// const thisWeek = dateArray.range(daysAgoFromToday, daysFromToday, "dddd", true)
 
+const mtwtfss = ["M", "T", "W", "T", "F", "S", "S"]
 export default class Calendar extends React.Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-    }
-  }
 
   render() {
     return (
       <div className="calendarPage">
-        {/* {momentToday}
-        Nästa datum är
-        {today.toLocaleString()}
-        {today.toLocaleString()} */}
-
-        {/* <div>
-          {daysInMonth.map(date => (
-@ -42,19 +41,35 @@ export default class Calendar extends React.Component {
-            <div className="calenderSquare">{date}</div>
-          ))}
-        </div> */}
-         {/* {this.props.toDoItems[0].checkedDates} */}
         <div className="calendarWrapper">
           <div className="calendarHeaderRow">
+
             <div className="taskInfo">!</div>
-            <div className="weekDay">M</div>
-            <div className="weekDay">T</div>
-            <div className="weekDay">W</div>
-            <div className="weekDay">T</div>
-            <div className="weekDay">F</div>
-            <div className="weekDay">S</div>
-            <div className="weekDay">S</div>
+
+            {mtwtfss.map(day => (
+              <div className="weekDay">{day}</div>
+            ))}
+
           </div>
 
           <div className="weekWrapper">
@@ -59,17 +33,10 @@ export default class Calendar extends React.Component {
                 symbol={item.symbol}
                 checkedDates={item.checkedDates}
                 kryss="o"
-                daysInWeek={daysInWeek}
-              />
+                daysInWeek={daysInWeek} />
             ))}
-            {/* {daysInWeek.map(date => (
-              <CalendarDays
-                dates={date}
-                kryss="hej"
-              />
-            ))}
-            ))} */}
           </div>
+
         </div>
       </div>
     )
--- code/src/components/calendar/calendarDays.js
+++ code/src/components/calendar/calendarDays.js
@@ -1,6 +1,4 @@
 import React from "react"
-import moment from "moment"
-import dateArray from "moment-array-dates"
 import "./calendar.css"
 
 export default class Calendar extends React.Component {
@@ -12,31 +10,22 @@ export default class Calendar extends React.Component {
     }
   }
 
-  // Ska skriva en fnuktion som mappar över veckans datum och
-  // letar efter dom i checkedDates.
-  // Sen storar dom i state och renderar dom.
   componentDidMount() {
-    console.log("the weeks dates:", this.props.daysInWeek)
-    console.log("the checked dates:", this.props.checkedDates)
+    // console.log("the weeks dates:", this.props.daysInWeek)
+    // console.log("the checked dates:", this.props.checkedDates)
     if (this.props.checkedDates) {
       const isMatch = this.props.daysInWeek.map(date => {
         return this.props.checkedDates.find(d => (
           d === date
         ))
       })
-      console.log("Matching dates", isMatch)
+      // console.log("Matching dates", isMatch)
       this.setState({
         weekMatches: isMatch
       })
       console.log("State dates", isMatch)
     }
   }
-
-  // addMatchedDate = dates => {
-  //   this.setState({
-  //     weekMatches: dates
-  //   })
-  // }
 
   render() {
     return (
@@ -46,27 +35,13 @@ export default class Calendar extends React.Component {
         </div>
 
         <div className="calendarLineRight">
-          <div className="calendarBox">
-            {this.state.weekMatches[0]}
-          </div>
-          <div className="calendarBox">
-            {this.state.weekMatches[1]}
-          </div>
-          <div className="calendarBox">
-            {this.state.weekMatches[2]}
-          </div>
-          <div className="calendarBox">
-            {this.state.weekMatches[3]}
-          </div>
-          <div className="calendarBox">
-            {this.state.weekMatches[4]}
-          </div>
-          <div className="calendarBox">
-            {this.state.weekMatches[5]}
-          </div>
-          <div className="calendarBox">
-            {this.state.weekMatches[6]}
-          </div>
+          {this.state.weekMatches.map(day => (
+            <div className="calendarBox">
+              {day !== undefined &&
+                <div>you did it! {this.props.id}</div>
+              }
+            </div>
+          ))}
         </div>
 
       </div>
