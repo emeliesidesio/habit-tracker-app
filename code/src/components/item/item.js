09eda92ada6c92dfc8817c8c236fcc1afc17e1f9
katudd <katrin@uddstromer.se>
2018-01-04 14:31:09 +0100

    Remove item bolder

--- code/src/components/item/item.js
+++ code/src/components/item/item.js
@@ -41,9 +41,9 @@ export default class Item extends React.Component {
           <div className="done-circle-checked">
             <input
               id="done-circle"
-              type="checkbox" />
-            {/* // checked={this.state.done}
-             // onChange={this.handleCheck} /> */}
+              type="checkbox"
+              checked={this.state.done}
+              onChange={this.handleCheck} />
             <label htmlFor="done-circle" />
           </div>
           <button className="remove" onClick={this.handleRemoveClick}><span className="remove-icon">-</span></button>
